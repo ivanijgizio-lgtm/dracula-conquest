896a354a9f726fae695210cd7ead696f132b8d77
Yan <ivanijgizio@gmail.com>
2026-07-29 13:31:51 +0000

    ПОБЕДА! Игра работает без ошибок стилей

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,2 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  plugins: [tailwindcss()],
-  server: {
-    open: true // Автоматически откроет браузер
-  }
-});
+export default defineConfig({});
